65f8e9bda4309bc4cbb18ad99520cf195cb65b2c
DiNerdyYawdie <62502925+DiNerdyYawdie@users.noreply.github.com>
2023-04-04 16:05:54 -0400

    Initial Commit

---
+++ Reusable Card View/Reusable_Card_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reusable_Card_ViewApp.swift
+//  Reusable Card View
+//
+//  Created by Chad on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Reusable_Card_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
